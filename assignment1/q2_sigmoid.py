319f28d96c5f534d65d4f500a82bb4e74d7a980c
erfan <mhi.erfan1@gmail.com>
2018-08-28 06:12:29 +0430

    q2_sigmoid done.

--- assignment1/q2_sigmoid.py
+++ assignment1/q2_sigmoid.py
@@ -15,7 +15,7 @@ def sigmoid(x):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    s = 1/(1+np.exp(-x))
     ### END YOUR CODE
 
     return s
@@ -35,7 +35,7 @@ def sigmoid_grad(s):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    ds = s*(1-s)
     ### END YOUR CODE
 
     return ds
@@ -46,21 +46,22 @@ def test_sigmoid_basic():
     Some simple tests to get you started.
     Warning: these are not exhaustive.
     """
-    print "Running basic tests..."
+    print ("Running basic tests...")
     x = np.array([[1, 2], [-1, -2]])
     f = sigmoid(x)
     g = sigmoid_grad(f)
-    print f
+    print (f)
     f_ans = np.array([
         [0.73105858, 0.88079708],
         [0.26894142, 0.11920292]])
     assert np.allclose(f, f_ans, rtol=1e-05, atol=1e-06)
-    print g
+    print (g)
     g_ans = np.array([
         [0.19661193, 0.10499359],
         [0.19661193, 0.10499359]])
+
     assert np.allclose(g, g_ans, rtol=1e-05, atol=1e-06)
-    print "You should verify these results by hand!\n"
+    print ("You should verify these results by hand!\n")
 
 
 def test_sigmoid():
@@ -70,10 +71,25 @@ def test_sigmoid():
     This function will not be called by the autograder, nor will
     your tests be graded.
     """
-    print "Running your tests..."
-    ### YOUR CODE HERE
-    raise NotImplementedError
-    ### END YOUR CODE
+    print ("Running your tests...")
+    x = np.array([[1, 2], [-1, -2], [1, 5], [-1, -8]])
+    f = sigmoid(x)
+    g = sigmoid_grad(f)
+    print (f)
+    f_ans = np.array([
+        [7.31058579e-01, 8.80797078e-01],
+        [2.68941421e-01, 1.19202922e-01],
+        [7.31058579e-01, 9.93307149e-01],
+        [2.68941421e-01, 3.35350130e-04]])
+    assert np.allclose(f, f_ans, rtol=1e-05, atol=1e-06)
+    print (g)
+    g_ans = np.array([
+        [0.19661193, 0.10499359],
+        [0.19661193, 0.10499359],
+        [0.19661193, 0.00664806],
+        [0.19661193, 0.00033524]])
+
+    assert np.allclose(g, g_ans, rtol=1e-05, atol=1e-06)
 
 
 if __name__ == "__main__":
